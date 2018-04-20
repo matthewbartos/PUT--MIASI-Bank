47735443db441bff42d8fdd406a5339f355070b6
Paulinson <mymedicine@op.pl>
2018-04-20 10:36:04 +0200

    IBankCreate interface

---
+++ ConsoleApp1/Bank Operations/IBankCreate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public interface IBankCreate
+    {
+        void SetOperationData(Client client = null, BankAccount account=null);
+
+        void Create();
+    }
+}
