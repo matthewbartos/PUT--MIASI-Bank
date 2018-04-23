e1d809598263472b0b2baf4224c06e440eef7116
Paulinson <mymedicine@op.pl>
2018-04-23 10:15:14 +0200

    PaymentOnAccount

---
+++ ConsoleApp1/Bank Operations/PaymentOnAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    class PaymentOnAccount : IBankOperation
+    {
+        private BankProduct bankProductDestination;
+        private float value;
+
+        public void Execute()
+        {
+            bankProductDestination.balance += value;
+        }
+
+        public void SetOperationData(BankProduct bankProductSource = null, BankProduct bankProductDestination = null, DateTime? date = null, float value = 0)
+        {
+            this.bankProductDestination = bankProductDestination;
+            this.value = value;
+        }
+    }
+}
