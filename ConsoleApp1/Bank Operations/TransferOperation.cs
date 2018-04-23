b026eb16572e34eebd62ee469ceeb63cb733d242
Paulinson <mymedicine@op.pl>
2018-04-23 09:56:17 +0200

    Withdraw + small changes

--- ConsoleApp1/Bank Operations/TransferOperation.cs
+++ ConsoleApp1/Bank Operations/TransferOperation.cs
@@ -16,7 +16,7 @@ namespace Bank
             bankProductDestination.balance += value;
         }
 
-        public void SetOperationData(BankProduct bankProductSource, BankProduct bankProductDestination, DateTime? date, float value)
+        public void SetOperationData(BankAccount bankProductSource, BankAccount bankProductDestination, DateTime? date, float value)
         {
             this.bankProductSource = bankProductSource;
             this.bankProductDestination = bankProductDestination;
--- ConsoleApp1/Bank Operations/WithdrawOperation.cs
+++ ConsoleApp1/Bank Operations/WithdrawOperation.cs
@@ -5,9 +5,31 @@ using System.Text;
 namespace Bank
 {
 
-    class WithdrawOperation : BankOperation
+    class WithdrawOperation : IBankOperation
     {
-        private BankProduct bankProductSource;
+        private BankAccount bankProductSource;
+        
         private DateTime date;
+        private float value;
+
+        public void Execute()
+        {
+            if (bankProductSource.debet != null && value > bankProductSource.balance && value + bankProductSource.debet.balance <= bankProductSource.debet.maxDebet)
+            {
+                bankProductSource.balance = 0;
+                bankProductSource.debet.balance += value - bankProductSource.balance;
+            }
+            else if (value <= bankProductSource.balance)
+            {
+                bankProductSource.balance -= value;
+            }
+        }
+
+        public void SetOperationData(BankAccount bankProductSource, BankAccount bankProductDestination, DateTime? date, float value)
+        {
+            this.bankProductSource = bankProductSource;
+            this.value = value;
+
+        }
     }
 }
