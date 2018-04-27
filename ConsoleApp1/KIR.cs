802aa318052c2fd9ab649c462e7289edcc6702d5
Mateusz Bartos <geekbajt@gmail.com>
2018-04-27 08:09:14 +0200

    Added KIR

---
+++ ConsoleApp1/KIR.cs
@@ -0,0 +1,34 @@
+﻿namespace Bank
+{
+    public sealed class KIR
+    {
+        private List<Bank> allBank;
+
+        private static readonly Lazy<KIR> lazy =
+            new Lazy<KIR>(() => new KIR());
+
+        public static KIR Instance => lazy.Value;
+
+        private KIR()
+        {
+            allBank = new List<Bank>();
+        }
+
+        public void registerBank(Bank bank) {
+            allBank.Add(bank);
+        }
+
+        private void accountExists(string bankIdSource, BankProduct bankProductSource)
+        {
+            //TODO - impelement
+            //check bank id in allBank and find account
+        }
+
+        public Void transferMoney(String bankIdSource, String bankIdDestination, BankProduct bankProductSource, BankProduct bankProductDestination, DateTime date, Float value)
+        {
+            // check if destination bank exists & destination account exists
+            // if no = return money to source account
+            // if yes = perform money transfer
+        }
+    }
+}
