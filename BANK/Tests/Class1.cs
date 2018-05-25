b20a56652f6ec9b6f2548efd666072488254a2b7
Mateusz Bartos <geekbajt@gmail.com>
2018-05-25 23:32:57 +0000

    Added Test Adapter

---
+++ BANK/Tests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Bank.Tests
+{
+    [TestFixture]
+    public class BankTest
+    {
+        private readonly Bank _bank;
+
+        public BankTest()
+        {
+            _bank = new Bank();
+        }
+
+        [Test]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = true;
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+    }
+}
